d9ce8394868de14e6b286cdd1e1dcd664ea2e4f5
Piotr Dudek <piotr72@gmail.com>
2020-12-13 21:49:45 -0500

    bootstrap header

--- employee-connect/src/App.js
+++ employee-connect/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
 import FormInput from "./components/FormInput";
 import UsersView from "./components/UsersView";
 
@@ -37,6 +38,7 @@ class App extends React.Component {
   render(){
       return (
         <div className="App">
+            <Header />
             <FormInput />
             <UsersView />
         </div>
---
+++ employee-connect/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header () {
+
+  return (
+  <div className="jumbotron jumbotron-fluid">
+    <div className="container dark">
+      <h1 className="display-4">Employee Directory</h1>
+      <p className="lead">Search employees and filter them by their names</p>
+    </div>
+</div>
+  )
+}
+
+export default Header;
