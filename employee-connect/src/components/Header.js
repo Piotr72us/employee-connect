e6be629de86d352b51f47a85169a38832673a93c
Piotr Dudek <piotr72@gmail.com>
2020-12-14 23:19:34 -0500

    jumbotron

--- employee-connect/src/components/Header.js
+++ employee-connect/src/components/Header.js
@@ -3,10 +3,10 @@ import React from "react";
 function Header () {
 
   return (
-  <div className="jumbotron jumbotron-fluid">
+  <div className="jumbotron jumbotron-fluid tester">
     <div className="container dark">
-      <h1 className="display-4">Employee Directory</h1>
-      <p className="lead">Search employees and filter them by their names</p>
+      <h1 className="display-4 lighter">Employee Directory</h1>
+      <p className="lead lighter">Search employees and filter them by their names</p>
     </div>
 </div>
   )
--- employee-connect/src/components/UsersView.js
+++ employee-connect/src/components/UsersView.js
@@ -5,9 +5,17 @@ function UsersView(props) {
   console.log("Here are our users", getUsers)
   // console.log("Here are our users", getUsers.results[0].name.first)
    // props: users (filteredUsers)
+  
    // button to filter by name 
       // onClick -> this.sortUsers
-  return <div>Users View</div>;
+  return (<div>
+    <h1>Users View</h1>
+    <h2>
+      <p>
+        {/* {getUsers.results[0].name.first} */}
+      </p>
+    </h2>
+    </div>);
 }
 
 export default UsersView;
